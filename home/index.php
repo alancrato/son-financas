8e0d44beab305c809a2d9dff9376b2e442f72954
Alan Rodrigues <alancrato@hotmail.com>
2018-03-08 14:36:27 -0300

    Add the fluxe two commit, not found commit next

--- home/index.php
+++ home/index.php
@@ -1,7 +1,22 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: alanc
- * Date: 08/03/2018
- * Time: 14:27
- */
+
+use SONFin\Application;
+use SONFin\Plugins\RoutePlugin;
+use SONFin\ServiceContainer;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$serviceContainer = new ServiceContainer();
+$app = new Application($serviceContainer);
+
+$app->plugin(new RoutePlugin());
+
+$app->get('/', function() {
+    echo "Hello World!";
+});
+
+$app->get('/route', function (){
+   echo "New Route";
+});
+
+$app->start();

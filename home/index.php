7749423f54eab89df56630f87786fe2d57bf482e
Alan Rodrigues <alancrato@hotmail.com>
2018-03-15 11:27:33 -0300

    Add repositories and aplication

--- home/index.php
+++ home/index.php
@@ -1,13 +1,10 @@
 <?php
 
-use Psr\Http\Message\RequestInterface;
-use Psr\Http\Message\ServerRequestInterface;
 use SONFin\Application;
 use SONFin\Plugins\DbPlugin;
 use SONFin\Plugins\RoutePlugin;
 use SONFin\Plugins\ViewPlugin;
 use SONFin\ServiceContainer;
-use Zend\Diactoros\Response;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
@@ -18,60 +15,28 @@ $app->plugin(new RoutePlugin());
 $app->plugin(new ViewPlugin());
 $app->plugin(new DbPlugin());
 
-/*$app->get('/{name}', function(ServerRequestInterface $request) use ($app) {
+/*
+$app->get('/{name}', function(ServerRequestInterface $request) use ($app) {
     $view = $app->service('view.renderer');
     return $view->render('test.html.twig', ['name' => $request->getAttribute('name')]);
-});*/
+});
+
+$app->get('/route', function (RequestInterface $request){
+   var_dump($request->getUri());die();
+   echo "New Route";
+});
 
 $app->get('/params/{name}/{id}', function(ServerRequestInterface $request) {
     $response = new Response();
     $response->getBody()->write("Response the emmiter to Diactoros");
     return $response;
 });
+*/
 
-$app->get('/', function(RequestInterface $request){
-    $request->getUri();
+$app->get('/', function(){
     return "Welcome to page index";
 });
 
-$app
-    ->get('/category-costs', function() use($app){
-        $meuModel = new \SONFin\Models\CategoryCosts();
-        $categories = $meuModel->all();
-        $view = $app->service('view.renderer');
-        return $view->render('category-costs/list.html.twig',[
-            'categories' => $categories
-        ]);
-    },'category-costs.list')
-    ->get('/category-costs/new', function() use($app){
-        $view = $app->service('view.renderer');
-        return $view->render('category-costs/create.html.twig');
-    },'category-costs.new')
-    ->post('/category-costs/store', function(ServerRequestInterface $request) use ($app){
-        $data = $request->getParsedBody();
-        SONFin\Models\CategoryCosts::create($data);
-        return $app->route('category-costs.list');
-    },'category-costs.store')
-    ->get('/category-costs/{id}/edit', function(ServerRequestInterface $request) use($app){
-        $view = $app->service('view.renderer');
-        $id = $request->getAttribute('id');
-        $category = \SONFin\Models\CategoryCosts::findOrFail($id);
-        return $view->render('category-costs/edit.html.twig', [
-            'category' => $category
-        ]);
-    },'category-costs.edit')
-    ->post('/category-costs/{id}/update', function(ServerRequestInterface $request) use($app){
-        $id = $request->getAttribute('id');
-        $category = \SONFin\Models\CategoryCosts::findOrFail($id);
-        $data = $request->getParsedBody('id');
-        $category->fill($data);
-        $category->save();
-        return $app->route('category-costs.list');
-    },'category-costs.update');
-
-$app->get('/route', function (RequestInterface $request){
-   var_dump($request->getUri());die();
-   echo "New Route";
-});
+require_once __DIR__ . '/../src/controllers/category-costs.php';
 
 $app->start();
--- src/Plugins/DbPlugin.php
+++ src/Plugins/DbPlugin.php
@@ -3,6 +3,9 @@ declare(strict_types=1);
 
 namespace SONFin\Plugins;
 
+use Interop\Container\ContainerInterface;
+use SONFin\Models\CategoryCosts;
+use SONFin\Repository\RepositoryFactory;
 use SONFin\ServiceContainerInterface;
 use Illuminate\Database\Capsule\Manager as Capsule;
 
@@ -15,5 +18,10 @@ class DbPlugin implements PluginInterface
         $config = include __DIR__ . '/../../config/db.php';
         $capsule->addConnection($config['development']);
         $capsule->bootEloquent();
+
+        $container->add('repository.factory', new RepositoryFactory());
+        $container->addLazy('category-costs.repository', function (ContainerInterface $container) {
+            return $container->get('repository.factory')->factory(CategoryCosts::class);
+        });
     }
 }
